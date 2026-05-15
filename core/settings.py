6a9baa755fc1a64d9d890f1f1700956667cae871
sairam-git-09 <psran999@gmail.com>
2026-05-15 12:18:43 +0530

    Change static files storage backend to CompressedStaticFilesStorage

--- core/settings.py
+++ core/settings.py
@@ -143,12 +143,12 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
 # Compatibility for cloudinary_storage
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 WHITENOISE_MANIFEST_STRICT = False
 
