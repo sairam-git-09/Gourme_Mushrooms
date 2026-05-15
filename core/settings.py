a2f197ed51bb73ce9ed6921c40726603ca0e6bef
sairam-git-09 <psran999@gmail.com>
2026-05-15 11:37:36 +0530

    Add media directory to STATICFILES_DIRS

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static' , BASE_DIR / 'media']
 
 # WhiteNoise Configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
