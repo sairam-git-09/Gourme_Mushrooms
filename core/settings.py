8c69cc4bb92b3386fb6b4dc43f0a507fc3994541
sairam-git-09 <psran999@gmail.com>
2026-05-15 12:00:38 +0530

    Refactor static files and storage configuration
    Updated STATICFILES_DIRS and restructured storage settings.

--- core/settings.py
+++ core/settings.py
@@ -134,11 +134,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static' , BASE_DIR / 'media']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # WhiteNoise Configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
